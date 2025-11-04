34ed4f987b55dad78f35e1b867152451916187bf
Mariel Gomez <marielgomezc1z@gmail.com>
2025-11-04 14:13:33 -0500

    changing deployment settings for index.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portfolio/", // <- add this line
+  base: "/portfolio/", 
   server: {
     host: "::",
     port: 8080,
